c679b2976d7c1a031d914aa8a5148f653bd641a8
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-04-04 21:08:43 -0300

    Exercicio01

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula02Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
+            int numero1 = 35;
+            int numero2 = 40;
+
+            double media = ((double)numero1 + (double)numero2) / 2;
+
+            Console.WriteLine("A média aritmética é: " + media);
+        }
+    }
+}
